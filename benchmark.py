dd43da15fd7a2e8fd41a18eac495027c0487a17e
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-08 01:20:15 +0300

    updates

--- benchmark.py
+++ benchmark.py
@@ -32,9 +32,9 @@ GENERATION_KWARGS = {
     "max_new_tokens": 64,
     'eos_token_id': 198,
     'do_sample': True,
-    # 'temperature': 0.72,
-    # 'top_k': 0,
-    # 'top_p': 0.725,
+    'temperature': 0.72,
+    'top_k': 0,
+    'top_p': 0.725,
     # 'repetition_penalty': 1.13,
 }
 
