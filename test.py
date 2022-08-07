70a5a8e63a3252c4292480ebe280efc6c7db18fc
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-07 15:46:18 +0300

    updates

--- test.py
+++ test.py
@@ -4,16 +4,17 @@
 # If you have a machine with a powerful GPU, I highly recommend you to try this example there!
 import time
 import tensorflow as tf
-from transformers import AutoTokenizer, TFAutoModelForCausalLM
+from transformers import AutoTokenizer, TFAutoModelForCausalLM, AutoModelForCausalLM
 
 # 1. Load model and tokenizer
 model_name = "gpt2" # hakurei/litv2-6B-rev2
 # remember: decoder-only models need left-padding
 tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left", pad_token="</s>")
 model = TFAutoModelForCausalLM.from_pretrained(model_name, from_pt=True)
+pt_model = AutoModelForCausalLM.from_pretrained(model_name)
 
 # 2. Prepare tokenization and generation arguments -- don't forget padding to avoid retracing!
-tokenization_kwargs = {"pad_to_multiple_of": 64, "padding": True, "return_tensors": "tf"}
+tokenization_kwargs = {"padding": True}
 generation_kwargs = {
     'temperature': 0.72,
     'repetition_penalty': 1.13125,
@@ -40,23 +41,23 @@ generation_kwargs = {
 xla_generate = tf.function(model.generate, jit_compile=True)
 
 # print("ORIGINAL GENERATE:")
-# # 4. Generate! Remember -- the first call will be slow, but all subsequent calls will be fast if you've done things right.
-# input_prompts = [f"The best thing about {country} is" for country in ["Spain", "Japan", "Angola"]]
-# for input_prompt in input_prompts:
-#     tokenized_inputs = tokenizer([input_prompt], **tokenization_kwargs)
-#     start = time.time_ns()
-#     generated_text = model.generate(**tokenized_inputs, **generation_kwargs)
-#     end = time.time_ns()
-#     decoded_text = tokenizer.decode(generated_text[0], skip_special_tokens=True)
-#     print(f"Original prompt -- {input_prompt}")
-#     print(f"Generated -- {decoded_text}")
-#     print(f"Execution time -- {(end - start) / 1e6:.1f} ms\n")
-# print("#" * 80)
+# 4. Generate! Remember -- the first call will be slow, but all subsequent calls will be fast if you've done things right.
+input_prompts = [f"The best thing about {country} is" for country in ["Spain", "Japan", "Angola"]]
+for input_prompt in input_prompts:
+    tokenized_inputs = tokenizer([input_prompt], **tokenization_kwargs, return_tensors="pt")
+    start = time.time_ns()
+    generated_text = pt_model.generate(**tokenized_inputs, **generation_kwargs)
+    end = time.time_ns()
+    decoded_text = tokenizer.decode(generated_text[0], skip_special_tokens=True)
+    print(f"Original prompt -- {input_prompt}")
+    print(f"Generated -- {decoded_text}")
+    print(f"Execution time -- {(end - start) / 1e6:.1f} ms\n")
+print("#" * 80)
 
 print("XLA GENERATE:")
 input_prompts = [f"The best thing about {country} is" for country in ["Spain", "Japan", "Angola"]]
 for input_prompt in input_prompts:
-    tokenized_inputs = tokenizer([input_prompt], **tokenization_kwargs)
+    tokenized_inputs = tokenizer([input_prompt], **tokenization_kwargs, return_tensors="tf")
     start = time.time_ns()
     generated_text = xla_generate(**tokenized_inputs, **generation_kwargs)
     end = time.time_ns()
