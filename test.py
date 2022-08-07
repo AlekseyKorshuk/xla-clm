70ea7bce0123cb91d46864fa9e41f72f321d4b8b
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-07 15:47:23 +0300

    updates

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from transformers import AutoTokenizer, TFAutoModelForCausalLM, AutoModelForCausalLM
 
 # 1. Load model and tokenizer
-model_name = "gpt2" # hakurei/litv2-6B-rev2
+model_name = "hakurei/litv2-6B-rev2" # hakurei/litv2-6B-rev2
 # remember: decoder-only models need left-padding
 tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left", pad_token="</s>")
 model = TFAutoModelForCausalLM.from_pretrained(model_name, from_pt=True)
@@ -32,7 +32,7 @@ generation_kwargs = {
     'temperature': 0.72,
     'top_k': 0,
     'top_p': 0.725,
-    'repetition_penalty': 1,
+    'repetition_penalty': 1.13,
 }
 
 
