fbe19d09a5b831b82269c61d25ccc04745992c35
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-07 15:30:10 +0300

    updates

--- test.py
+++ test.py
@@ -24,6 +24,8 @@ generation_kwargs = {
     'eos_token_id': 198,
     # 'bad_words_ids': self.bad_words_ids
 }
+generation_kwargs = {"num_beams": 4, "max_new_tokens": 32}
+
 
 # 3. Create your XLA generate function a̶n̶d̶ ̶m̶a̶k̶e̶ ̶P̶y̶T̶o̶r̶c̶h̶ ̶e̶a̶t̶ ̶d̶u̶s̶t̶
 # This is the only change with respect to original generate workflow!
