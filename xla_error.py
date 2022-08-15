8602e5eb3719fbc8b2128124352714b1d3f7059c
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-15 15:53:55 +0300

    updates

--- xla_error.py
+++ xla_error.py
@@ -4,16 +4,23 @@ from transformers import AutoTokenizer, TFAutoModelForCausalLM
 
 generation_kwargs = {
     "max_new_tokens": 64,
-    'eos_token_id': 198,
-    'do_sample': True,
-    'temperature': 0.72,
-    'top_k': 0,
-    'top_p': 0.725,
-    'repetition_penalty': 1.13,
+    "eos_token_id": 198,
+    "do_sample": True,
+    "temperature": 0.72,
+    "top_k": 0,
+    "top_p": 0.725,
+    "repetition_penalty": 1.13,
+}
+
+padding_kwargs = {
+    "pad_to_multiple_of": 8,
+    "padding": True,
+    "padding_side": "left",
+    "pad_token": "</s>",
 }
 
 tokenizer = AutoTokenizer.from_pretrained(
-    "gpt2", padding_side="left", pad_token="</s>"
+    "gpt2", **padding_kwargs
 )
 model = TFAutoModelForCausalLM.from_pretrained("gpt2")
 model.config.pad_token_id = model.config.eos_token_id
