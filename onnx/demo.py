e64af6d10ffcc828222701f9ca04658686e69ff6
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-29 11:37:54 +0300

    updates

--- onnx/demo.py
+++ onnx/demo.py
@@ -1,7 +1,17 @@
-from optimum_transformers import pipeline
+# from optimum_transformers import pipeline
+#
+# # Initialize a pipeline by passing the task name and
+# # set onnx to True (default value is also True)
+# nlp = pipeline("text-generation", "gpt2", use_onnx=True)
+# nlp("Transformers and onnx runtime is an awesome")
+# # [{'label': 'POSITIVE', 'score': 0.999721109867096}]
 
-# Initialize a pipeline by passing the task name and
-# set onnx to True (default value is also True)
-nlp = pipeline("text-generation", "gpt2", use_onnx=True)
-nlp("Transformers and onnx runtime is an awesome")
-# [{'label': 'POSITIVE', 'score': 0.999721109867096}]
+from optimum_transformers import Benchmark
+
+task = "text-generation"
+model_name = "gpt2"
+num_tests = 3
+
+benchmark = Benchmark(task, model_name)
+results = benchmark(num_tests, plot=True)
+print(results)
