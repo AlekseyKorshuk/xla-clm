be4382e91af6d84b755aff6805a9031008aef048
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-30 23:22:37 +0300

    updates

--- onnx/demo.py
+++ onnx/demo.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from onnxruntime import InferenceSession
 import tqdm
 
@@ -7,6 +7,14 @@ session = InferenceSession("onnx/model.onnx", providers=["CUDAExecutionProvider"
 # ONNX Runtime expects NumPy arrays as input
 inputs = tokenizer("Using DistilBERT with ONNX Runtime!", return_tensors="np")
 
+print("ONNX")
 for _ in tqdm.trange(10):
     for _ in tqdm.trange(10):
         outputs = session.run(output_names=["last_hidden_state"], input_feed=dict(inputs))
+
+print("Pytorch")
+model = AutoModelForCausalLM.from_pretrained("hakurei/litv2-6B-rev2").to(0)
+inputs = tokenizer("Using DistilBERT with ONNX Runtime!", return_tensors="pt").to(0)
+for _ in tqdm.trange(10):
+    for _ in tqdm.trange(10):
+        outputs = model(**inputs)
