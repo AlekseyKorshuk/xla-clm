35001d5fabed67990279378b735fc811e4133d09
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-31 19:34:18 +0300

    updates

--- deepspeed /bert.py
+++ deepspeed /bert.py
@@ -21,6 +21,7 @@ torch_pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, devic
 # Test pipeline
 GENERATION_KWARGS = {
     "max_new_tokens": 32,
+    "min_new_tokens": 4,
     'eos_token_id': 198,
     'do_sample': True,
     'pad_token_id': 198,
