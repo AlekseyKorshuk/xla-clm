77416ef5c92578a9a723a03fe21006d0112d48b2
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-31 20:03:04 +0300

    updates

--- deepspeed /bert.py
+++ deepspeed /bert.py
@@ -46,7 +46,7 @@ ds_model = deepspeed.init_inference(
     dtype=torch.float32,  # dtype of the weights (fp16)
     # injection_policy={"BertLayer" : HFBertLayerPolicy}, # replace BertLayer with DS HFBertLayerPolicy
     # replace_method="auto",  # Lets DS autmatically identify the layer to replace
-    replace_with_kernel_inject=True,  # replace the model with the kernel injector
+    # replace_with_kernel_inject=True,  # replace the model with the kernel injector
 )
 
 # create acclerated pipeline
