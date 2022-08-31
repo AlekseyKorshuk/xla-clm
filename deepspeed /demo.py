ec443f9600ab1ce022d85909863325e1d61d15d8
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-31 14:35:02 +0300

    updates

---
+++ deepspeed /demo.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
+from transformers import pipeline
+from deepspeed.module_inject import HFBertLayerPolicy
+import deepspeed
+
+# Model Repository on huggingface.co
+model_id = "dslim/bert-large-NER"
+
+# load model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForTokenClassification.from_pretrained(model_id)
+
+# init deepspeed inference engine
+ds_model = deepspeed.init_inference(
+    model=model,  # Transformers models
+    mp_size=1,  # Number of GPU
+    dtype=torch.half,  # dtype of the weights (fp16)
+    # injection_policy={"BertLayer" : HFBertLayerPolicy}, # replace BertLayer with DS HFBertLayerPolicy
+    replace_method="auto",  # Lets DS autmatically identify the layer to replace
+    replace_with_kernel_inject=True,  # replace the model with the kernel injector
+)
+
+# create acclerated pipeline
+ds_clf = pipeline("token-classification", model=ds_model, tokenizer=tokenizer, device=0)
+
+# Test pipeline
+example = "My name is Wolfgang and I live in Berlin"
+ner_results = ds_clf(example)
+print(ner_results)
