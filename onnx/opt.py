c37363ea4cdf3f3764035f0b0a17d9168113e2de
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-31 11:17:51 +0300

    updates

--- onnx/opt.py
+++ onnx/opt.py
@@ -1,4 +1,6 @@
 import os
+
+import tqdm
 from optimum.onnxruntime import ORTModelForSequenceClassification, ORTModelForCausalLM
 from transformers import AutoTokenizer, pipeline
 
@@ -20,3 +22,11 @@ cls_pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer)
 
 results = cls_pipeline("I love burritos!")
 print(results)
+
+inputs = tokenizer("Using DistilBERT with ONNX Runtime!", return_tensors="np")
+
+print("ONNX")
+for _ in tqdm.trange(10):
+    for _ in tqdm.trange(10):
+        model(**inputs)
+        # outputs = session.run(output_names=["last_hidden_state"], input_feed=dict(inputs))
