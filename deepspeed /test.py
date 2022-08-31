b7b79b7a5b170b4cd7998b83c32ca4246139207b
Aleksey Korshuk <ale-kor02@mail.ru>
2022-08-31 23:38:33 +0300

    updates

--- deepspeed /test.py
+++ deepspeed /test.py
@@ -1,14 +1,15 @@
-# Filename: gpt-neo-2.7b-generation.py
 import os
 import deepspeed
 import torch
 from transformers import pipeline
-from datasets import load_dataset
 
-dataset = load_dataset("ChaiML/user_model_inputs")
+prompt = """Eliza is a therapist and she loves to listen and help.
 
-INPUT_EXAMPLES = dataset["train"]["text"][:10]
-
+Eliza: Hi, my name is Eliza. What is weighing on your mind?
+Me: Hey Eliza
+Eliza: Hi. I'm a therapist. How are you feeling?
+User: I am feeling lonely
+Eliza:"""
 
 local_rank = int(os.getenv('LOCAL_RANK', '0'))
 world_size = int(os.getenv('WORLD_SIZE', '1'))
@@ -21,6 +22,8 @@ generator.model = deepspeed.init_inference(generator.model,
                                            replace_method='auto',
                                            replace_with_kernel_inject=True)
 
-for example in INPUT_EXAMPLES:
-    string = generator(example, do_sample=False, max_new_tokens=50)[0]["generated_text"][len(example):]
-    print(string)
+generated_text = generator("Deepspeed is", do_sample=False, max_new_tokens=50, eos_token_id=198)[0]["generated_text"]
+print(generated_text)
+
+generated_text = generator(prompt, do_sample=False, max_new_tokens=50, eos_token_id=198)[0]["generated_text"]
+print(generated_text)
