35be962ee7bd124650c71e014c39cf064bdb0790
Aleksey Korshuk <ale-kor02@mail.ru>
2022-09-01 18:34:27 +0300

    updates

--- deepspeed /reward.py
+++ deepspeed /reward.py
@@ -6,31 +6,35 @@ import deepspeed
 import tqdm
 from datasets import load_dataset
 import warnings
+from datasets import load_dataset
+from evaluate import evaluator
+from transformers import AutoModelForSequenceClassification, pipeline
 
 warnings.filterwarnings("ignore")
-dataset = load_dataset("ChaiML/user_model_inputs")
+data = load_dataset("ChaiML/rm_train_dataset", split="validation").shuffle(seed=42).select(range(1000))
 # Model Repository on huggingface.co
 model_id = "ChaiML/reward_48m_gpt2_target_2"
 tokenizer_id = "gpt2"
 token = "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"
 # model_id = "gpt2"
 
+task_evaluator = evaluator("text-classification")
+
+
 # load model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_id, use_auth_token=token)
 model = AutoModelForSequenceClassification.from_pretrained(model_id, use_auth_token=token).to(0)
 
-INPUT_EXAMPLES = dataset["train"]["reward_input"][:10000]
-
 torch_pipe = pipeline("text-classification", model=model, tokenizer=tokenizer, device=0)
 
 print("Pytorch")
-torch_outputs = []
-for example in tqdm.tqdm(INPUT_EXAMPLES, desc="Pytorch"):
-    # inputs = tokenizer(example, return_tensors='pt').to(0)
-    # result = model.generate(**inputs, **GENERATION_KWARGS)
-    torch_output = torch_pipe(example)[0]['label']
-    torch_outputs.append(torch_output)
-# print(torch_output)
+eval_results = task_evaluator.compute(
+    model_or_pipeline=torch_pipe,
+    data=data,
+    # label_mapping={"NEGATIVE": 0, "POSITIVE": 1}
+)
+print(eval_results)
+
 # init deepspeed inference engine
 ds_model = deepspeed.init_inference(
     model=model,  # Transformers models
@@ -43,11 +47,11 @@ ds_model = deepspeed.init_inference(
 # create acclerated pipeline
 ds_clf = pipeline("text-classification", model=ds_model, tokenizer=tokenizer, device=0)
 
+
 print("Accelerated")
-accelerated_outputs = []
-for example in tqdm.tqdm(INPUT_EXAMPLES, desc="Accelerated"):
-    accelerated_output = ds_clf(example)[0]['label']
-    accelerated_outputs.append(accelerated_output)
-
-
-
+eval_results = task_evaluator.compute(
+    model_or_pipeline=ds_clf,
+    data=data,
+    # label_mapping={"NEGATIVE": 0, "POSITIVE": 1}
+)
+print(eval_results)
